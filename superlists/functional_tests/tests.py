934ef34a049f687947536957a3505688a4f42d24
kdchaires <karina.chaires@cimat.mx>
2016-11-12 21:07:26 -0600

    Use Bootstrap to improve layout

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -1,15 +1,16 @@
-from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.keys import Keys
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
 
 	def setUp(self):
 		self.browser = webdriver.Firefox()
 		self.browser.implicitly_wait(3)
 
 	def tearDown(self):
+		self.browser.refresh()
 		self.browser.quit()
 
 	def check_for_row_in_list_table(self, row_text):
@@ -61,6 +62,7 @@ class NewVisitorTest(LiveServerTestCase):
 
 		## We use a new browser session to make sure that no information
 		## of Edith's is comming through from cookies etc
+		self.browser.refresh()
 		self.browser.quit()
 		self.browser = webdriver.Firefox()
 
@@ -98,7 +100,17 @@ class NewVisitorTest(LiveServerTestCase):
 		self.assertAlmostEqual(
 			inputbox.location['x'] + inputbox.size['width'] / 2,
 			512,
-			delta=5
+			delta=8
+		)
+
+        # She starts a new list and sees the input is nicely
+        # centered there too
+		inputbox.send_keys('testing\n')
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertAlmostEqual(
+			inputbox.location['x'] + inputbox.size['width'] / 2,
+			512,
+			delta=8
 		)
 
 		# Edith wonders whether the site will remember her list. Then she sees
