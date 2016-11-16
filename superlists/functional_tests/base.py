42eceef9a16b9bd6a7e2371a21c559a538c4defc
kdchaires <karina.chaires@cimat.mx>
2016-11-15 23:54:36 -0600

    Adjust new list view to do model validation

--- superlists/functional_tests/base.py
+++ superlists/functional_tests/base.py
@@ -22,7 +22,8 @@ class FunctionalTest(StaticLiveServerTestCase):
 
 	def setUp(self):
 		self.browser = webdriver.Firefox()
-		self.browser.implicitly_wait(3)
+		#self.browser.implicitly_wait(3)
+		self.browser.set_page_load_timeout(30)
 
 
 	def tearDown(self):
@@ -31,6 +32,9 @@ class FunctionalTest(StaticLiveServerTestCase):
 
 
 	def check_for_row_in_list_table(self, row_text):
+		self.browser.implicitly_wait(100)
 		table = self.browser.find_element_by_id('id_list_table')
 		rows = table.find_elements_by_tag_name('tr')
+		self.browser.implicitly_wait(100)
 		self.assertIn(row_text, [row.text for row in rows])
+		self.browser.implicitly_wait(100)
--- superlists/lists/urls.py
+++ superlists/lists/urls.py
@@ -1,7 +1,8 @@
-from django.conf.urls import patterns,include, url
+from django.conf.urls import url
+from lists import views
 
-urlpatterns = patterns('',
-    url(r'^$', 'lists.views.home_page', name='home'),
-    url(r'^lists/', include('lists.urls')),
-    # url(r'^admin/', include(admin.site.urls)),
-)
+urlpatterns = [
+    url(r'^new$', views.new_list, name='new_list'),
+    url(r'^(\d+)/$', views.view_list, name='view_list'),
+    url(r'^(\d+)/add_item$', views.add_item, name='add_item'),
+]
